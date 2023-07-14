ce0eeaf1c0e1e9978d2b38aea648b8ef5e1dbda1
James M Thankachen <jamesmanjada@gmail.com>
2023-07-14 17:51:59 +0530

    Initial commt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import "./App.css";
+
+import React from "react";
 
 function App() {
+  const name = "James"
+  const score = 80;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="App">
+        <h1>Hello from React</h1>
+      </div>
+      <div className="User">
+        <h1>Hello {name}</h1>
+        <p>You have scored {0 + score}, Status is: {score > 60 ? 'Pass' : 'Fail'}</p>
+      </div>
+      </React.Fragment>
   );
 }
 
 export default App;
+
+//We are not using logo.svg, app.css, app.test.js etc.. so commenting
